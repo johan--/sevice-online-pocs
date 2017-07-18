d672b0aa727b229cf87ac93438f15157f047b4bc
Edmond Talla Ouafeu <fixed-term.Edmond.TallaOuafeu@bosch-si.com>
2017-07-18 15:54:50 +0200

    add constants routing

---
+++ frontend-angular/uber/constants.ts
@@ -0,0 +1,17 @@
+
+export const Constants = {
+  routing: {
+    home: '/home',
+    userLogin: '/user/login',
+    userLogout: '/user/logout',
+    userInfo: '/user-internal/info',
+    userSettings: '/user-internal/settings',
+    changePassword: '/user/changePassword',
+    forgotPassword: '/user/forgotPassword',
+    error: '/error'
+  },
+  errors: {
+    loginCheckFailed: 'Failed to check the Login',
+    resourceNotFound: 'Oops, the resource you requested was not found.'
+  }
+};
