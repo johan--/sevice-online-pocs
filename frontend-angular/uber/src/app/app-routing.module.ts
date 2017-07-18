bbec2cc487dd999f6c20b4f43db430572c8eec40
Edmond Talla Ouafeu <fixed-term.Edmond.TallaOuafeu@bosch-si.com>
2017-07-18 15:53:25 +0200

    add main-dashboard

--- frontend-angular/uber/src/app/app-routing.module.ts
+++ frontend-angular/uber/src/app/app-routing.module.ts
@@ -3,11 +3,33 @@ import { Routes, RouterModule } from '@angular/router';
 import {ExternalLayoutComponent} from './shared/external-layout/external-layout.component';
 import {InternalLayoutComponent} from './shared/internal-layout/internal-layout.component';
 import {ErrorComponent} from './shared/error/error.component';
+import {Constants} from "../../constants";
+import {MainDashboardComponent} from "./shared/main-dashboard/main-dashboard.component";
+
+export const HomeRoutesGenerator = {
+  home: function () {
+    return Constants.routing.home;
+  }
+};
 
 const routes: Routes = [
   {
     path: '',
-    children: []
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: InternalLayoutComponent,
+    children: [
+      {
+        path: 'home',
+        component: MainDashboardComponent,
+        data: {
+          title: 'Home'
+        }
+      },
+    ]
   }
 ];
 
--- frontend-angular/uber/src/app/shared/shared.module.ts
+++ frontend-angular/uber/src/app/shared/shared.module.ts
@@ -4,11 +4,12 @@ import { ExternalLayoutComponent } from './external-layout/external-layout.compo
 import { InternalLayoutComponent } from './internal-layout/internal-layout.component';
 import { TopNavComponent } from './top-nav/top-nav.component';
 import { ErrorComponent } from './error/error.component';
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ExternalLayoutComponent, InternalLayoutComponent, TopNavComponent, ErrorComponent]
+  declarations: [ExternalLayoutComponent, InternalLayoutComponent, TopNavComponent, ErrorComponent, MainDashboardComponent]
 })
 export class SharedModule { }
